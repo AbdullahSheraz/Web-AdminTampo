d107b14c82e9f596d35443044795087b08cbf6cb
me <muzammilmughal182@gmail.com>
2025-04-30 13:12:03 +0500

    repo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3560563519"
+    serviceWorkerVersion: "2513550498"
   }
 });
