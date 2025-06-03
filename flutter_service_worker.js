08b539d2e7a35086f7e2d989a7a2c865d5b4755b
me <muzammilmughal182@gmail.com>
2025-06-03 13:24:26 +0500

    repo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "85f53cf865f07e907a1d1e581139f329
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ccc02dd1cf74a7f5c9e84af18550b0db",
+"flutter_bootstrap.js": "2aaeaac36e8dd95e377faec3f60d9518",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
